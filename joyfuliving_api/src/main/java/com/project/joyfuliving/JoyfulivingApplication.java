477ff54073c96b98f0853bb34562e92a29b31873
Fatima Ezzahra Bakir <fatima-ezzahra.bakir@sogeti.com>
2021-12-03 15:53:53 +0100

    init api

---
+++ joyfuliving_api/src/main/java/com/project/joyfuliving/JoyfulivingApplication.java
@@ -0,0 +1,13 @@
+package com.project.joyfuliving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoyfulivingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoyfulivingApplication.class, args);
+	}
+
+}
---
+++ joyfuliving_api/src/test/java/com/project/joyfuliving/JoyfulivingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.joyfuliving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoyfulivingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
